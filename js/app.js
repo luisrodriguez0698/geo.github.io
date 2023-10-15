c8b7953ea10a2f836f28c90d281fd3a5f61165b3
luisrodriguez0698 <eduardo.rodriguez@cpavs.mx>
2023-10-14 18:19:18 -0600

    repo

---
+++ js/app.js
@@ -0,0 +1,89 @@
+    var love = setInterval(function(){
+    var r_num = Math.floor(Math.random() * 40) + 1;
+      var r_size = Math.floor(Math.random() * 65) + 10;
+      var r_left = Math.floor(Math.random() * 100) + 1;
+      var r_bg = Math.floor(Math.random() * 25) + 100;
+    var r_time = Math.floor(Math.random() * 10) + 5;
+    
+    $('.bg_heart').append("<div class='heart' style='width:"+r_size+"px;height:"+r_size+"px;left:"+r_left+"%;background:rgba(255,"+(r_bg-25)+","+r_bg+",1);-webkit-animation:love "+r_time+"s ease;-moz-animation:love "+r_time+"s ease;-ms-animation:love "+r_time+"s ease;animation:love "+r_time+"s ease'></div>");
+    
+    $('.bg_heart').append("<div class='heart' style='width:"+(r_size-10)+"px;height:"+(r_size-10)+"px;left:"+(r_left+r_num)+"%;background:rgba(255,"+(r_bg-25)+","+(r_bg+25)+",1);-webkit-animation:love "+(r_time+5)+"s ease;-moz-animation:love "+(r_time+5)+"s ease;-ms-animation:love "+(r_time+5)+"s ease;animation:love "+(r_time+5)+"s ease'></div>");
+    
+    $('.heart').each(function(){
+      var top = $(this).css("top").replace(/[^-\d\.]/g, '');
+      var width = $(this).css("width").replace(/[^-\d\.]/g, '');
+      if(top <= -100 || width >= 150){
+        $(this).detach();
+      }
+    });
+  },1000);
+
+
+
+  let currentIndex = 0;
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+    if (index < 0) {
+      currentIndex = totalSlides - 1;
+    } else if (index >= totalSlides) {
+      currentIndex = 0;
+    } else {
+      currentIndex = index;
+    }
+
+    const translateValue = -currentIndex * 100 + '%';
+    document.querySelector('.slider-content').style.transform = 'translateX(' + translateValue + ')';
+  }
+
+  function prevSlide() {
+    showSlide(currentIndex - 1);
+  }
+
+  function nextSlide() {
+    showSlide(currentIndex + 1);
+  }
+
+
+
+
+
+//   var tag = document.createElement('script');
+//   tag.src = 'https://www.youtube.com/iframe_api';
+//   var firstScriptTag = document.getElementsByTagName('script')[0];
+//   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//   var player;
+
+//   function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//       height: '315',
+//       width: '560',
+//       videoId: 'PmN7aT_mBkY', // Reemplaza con el ID de tu video de YouTube
+//       playerVars: {
+//         'autoplay': 1,
+//         'loop': 1,
+//         'controls': 0,
+//         'showinfo': 0,
+//         'rel': 0
+//       },
+//       events: {
+//         'onReady': onPlayerReady
+//       }
+//     });
+//   }
+
+//   function onPlayerReady(event) {
+//     event.target.playVideo();
+//   }
+
+
+
+
+
+  
+  
+  
+  
+  
